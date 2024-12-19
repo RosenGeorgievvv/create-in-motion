543e58fbc464b68e032c70fb750dbaebafb636e4
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-12-19 15:33:08 +0200

    update

--- src/components/data.js
+++ src/components/data.js
@@ -1,8 +1,16 @@
+import rwanda1 from '../assets/rwanda/rwanda1.jpg';
+import rwanda2 from '../assets/rwanda/rwanda2.jpg';
+import rwanda3 from '../assets/rwanda/rwanda3.jpg';
+import rwanda4 from '../assets/rwanda/rwanda4.jpg';
+import rwanda5 from '../assets/rwanda/rwanda5.jpg';
+
 const data = [
     {
       id: 1,
       name: "Rwanda & Burundi",
-      img: "https://example.com/kigali-genocide-memorial.jpg",
+      img: [
+        rwanda1, rwanda2, rwanda3, rwanda4, rwanda5
+      ],
       description:
         "Rwanda and Burundi are the second and third of five countries featured in a journey called Around Lake Victoria.These two small nations, standing off the beaten tourist path, offer an authentic African experience, blessed with dramatic landscapes, rich biodiversity, and lakeside communities, each with its unique culture and a tragic history stained by brutal ethnic conflicts. Rwanda is healing the wounds of the 1994 genocide and transforming into a model of national unity and economic prosperity. From the Genocide Memorial in the pristine capital Kigali, along its impeccable roads, to the fishing villages around Lake Kivu and Nyungwe Forest National Park—home to Africa's oldest tropical forest, situated at the watershed between the Congo River basin to the west and the Nile River basin to the east.Burundi, meanwhile, is plagued by conflict, with poverty as the norm for all but a few. From Lake Tanganyika and the capital Bujumbura—surprisingly boasting refined restaurants and chic nightclubs—to Rusizi National Park and the site where journalist Henry Morton Stanley met explorer David Livingstone and uttered the iconic phrase,Dr. Livingstone, I presume?—Burundi offers a compelling mix of history and culture. Add to that the village of Gishora, where the captivating performances of the former royal drummers continue to enchant visitors.",
       type: "Rwanda",
