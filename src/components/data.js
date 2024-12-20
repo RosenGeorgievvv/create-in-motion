f5443821747742701c8678aefb7b9bf135de7abb
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-12-20 15:28:45 +0200

    Update data.js

--- src/components/data.js
+++ src/components/data.js
@@ -18,6 +18,26 @@ import yemen4 from '../assets/yemen/yemen4.jpg';
 import yemen5 from '../assets/yemen/yemen5.jpg';
 import yemen6 from '../assets/yemen/yemen6.jpg';
 
+import venezuela1 from '../assets/venezuela/venezuela1.jpg';
+import venezuela2 from '../assets/venezuela/venezuela2.jpg';
+import venezuela3 from '../assets/venezuela/venezuela3.jpg';
+import venezuela4 from '../assets/venezuela/venezuela4.jpg';
+import venezuela5 from '../assets/venezuela/venezuela5.jpg';
+import venezuela6 from '../assets/venezuela/venezuela6.jpg';
+import venezuela7 from '../assets/venezuela/venezuela7.jpg';
+import venezuela8 from '../assets/venezuela/venezuela8.jpg';
+import venezuela9 from '../assets/venezuela/venezuela9.jpg';
+import venezuela10 from '../assets/venezuela/venezuela10.jpg';
+
+import pakistan1 from '../assets/pakistan/pakistan1.jpg';
+import pakistan2 from '../assets/pakistan/pakistan2.jpg';
+import pakistan3 from '../assets/pakistan/pakistan3.jpg';
+import pakistan4 from '../assets/pakistan/pakistan4.jpg';
+import pakistan5 from '../assets/pakistan/pakistan5.jpg';
+import pakistan6 from '../assets/pakistan/pakistan6.jpg';
+import pakistan7 from '../assets/pakistan/pakistan7.jpg';
+import pakistan8 from '../assets/pakistan/pakistan8.jpg';
+
 const data = [
   {
     id: 1,
@@ -43,17 +63,16 @@ const data = [
   {
     id: 4,
     name: "Venezuela",
-    img: [rwanda1, rwanda2, rwanda3, rwanda4, rwanda5],
+    img: [venezuela1, venezuela2, venezuela3, venezuela4, venezuela5, venezuela6, venezuela7, venezuela8, venezuela9, venezuela10],
     description:
       "In ancient times, when America and Africa were part of a single continent, strange mountain plateaus formed, rising high above the surrounding landscape. Today, the jungle covering the land all around is home to the Pemón Indians, who believe that spirits called Mawari live at the peaks. They refer to these flat mountains as tepuis. They also call them islands in the sky. I decided to wander through the lands of the Pemón and meet their spirits somewhere in the southeastern part of Venezuela, where Canaima National Park is located. This is the home of the iconic tepuis with flat tops, which inspired Sir Arthur Conan Doyle to write The Lost World. An unreal place, where the world’s highest waterfall, Angel Falls (Salto Ángel), drops from nearly a kilometer in height. Climbing Mount Roraima was a must. It is said to be one of the best hiking routes in South America. I added a boat trip through the Orinoco Delta and encounters with the Warrau Indians, whose way of life is far from our own. To finish, I needed to unwind on Isla Margarita. After all, life in the jungle is tough... The itinerary was set. So, with a group of friends, I wandered through Venezuela...",
   },
-  //uganda
   {
     id: 5,
-    name: "Uganda",
-    img: [uganda1, uganda2, uganda3, uganda4, uganda5, uganda6],
+    name: "Pakistan",
+    img: [pakistan1, pakistan2, pakistan3, pakistan4, pakistan5, pakistan6, pakistan7, pakistan8],
     description:
-      "Rwanda and Burundi are the second and third of five countries featured in a journey called Around Lake Victoria.These two small nations, standing off the beaten tourist path, offer an authentic African experience, blessed with dramatic landscapes, rich biodiversity, and lakeside communities, each with its unique culture and a tragic history stained by brutal ethnic conflicts. Rwanda is healing the wounds of the 1994 genocide and transforming into a model of national unity and economic prosperity. From the Genocide Memorial in the pristine capital Kigali, along its impeccable roads, to the fishing villages around Lake Kivu and Nyungwe Forest National Park—home to Africa's oldest tropical forest, situated at the watershed between the Congo River basin to the west and the Nile River basin to the east.Burundi, meanwhile, is plagued by conflict, with poverty as the norm for all but a few. From Lake Tanganyika and the capital Bujumbura—surprisingly boasting refined restaurants and chic nightclubs—to Rusizi National Park and the site where journalist Henry Morton Stanley met explorer David Livingstone and uttered the iconic phrase,Dr. Livingstone, I presume?—Burundi offers a compelling mix of history and culture. Add to that the village of Gishora, where the captivating performances of the former royal drummers continue to enchant visitors.",
+      "After the stroll through Afghanistan, the one in Pakistan felt calm, like a provincial Scandinavian town.From Lahore and the grandeur of the Mughal heritage to Islamabad, built meticulously according to the principles of ekistics. From the Pashtun city of Peshawar to the Gilgit-Baltistan province, blessed with incredible ethnic diversity and natural beauty. I lived with the people of the Hunza Valley, drank tea on the Fairy Meadows beneath Nanga Parbat, and climbed to the base camp of the peak. I traveled the Karakoram Highway and reached the Khunjerab Pass at the border with China. I saw the majestic Indus River and the place where the Hindu Kush, Karakoram, and Himalayas—Earth's three tallest mountain ranges—meet.Satisfied, I returned home.It was a memorable journey.The year continues.Is there a desire for more...?",
   },
   {
     id: 6,
